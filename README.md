199233c81ad27d5179f225b8f975318f781d3fc6
Abhinav Gupta <abhig0502@gmail.com>
2024-10-22 08:41:04 +0530

    playing with the routes

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ how this const app=express() is working what is it doing ?
 issues need to be resolved--
 
 function handler is not working properly
+
+what is the difference between req.query and req.params we can get the data from both of them 
--- src/app.js
+++ src/app.js
@@ -2,20 +2,29 @@ const express = require("express");
 
 const app = express();
 
+// app.get("/user/:userId/:name/:password", (req, res) => {   //http://localhost:7777/user/711/abhinav/6789873
+//     console.log(req.params);
+//   res.send({ FirstName: "Abhinav", LastName: "Gupta" });
+// });
+
+// app.get("/user", (req, res) => {        //http://localhost:7777/user?userId=1010&name=Abhinav&password=Sainath
+//     console.log(req.query);
+//   res.send({ FirstName: "Abhinav", LastName: "Gupta" });
+// });
+
 app.get("/user", (req, res) => {
   res.send({ FirstName: "Abhinav", LastName: "Gupta" });
 });
 
 app.post("/user", (req, res) => {
-    //store the data to the DB
+  //store the data to the DB
   res.send("data has been saved successfully ");
 });
 
 app.delete("/user", (req, res) => {
-    //delete data from the DB
+  //delete data from the DB
   res.send("data has been deleted successfully ");
 });
-
 
 app.get("/", (req, res) => {
   res.send("Hello from the Dashboard!"); //this function is known as request handler
